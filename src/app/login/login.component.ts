ec742bc4df40b59b7e81d534c0829cdc05ff6f3e
unknown <dinodavis18@gmail.com>
2023-01-31 09:59:08 +0530

    second bank commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,8 +9,8 @@ export class LoginComponent implements OnInit {
 
   data="Your Perfect Banking Partner"
 
-  acno=""
-  psw=""
+  // acno=""
+  // psw=""
 
   userDetails:any={
     1000:{acno:1000,username:"anu",password:"abc123",balance:0},
@@ -24,15 +24,46 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  login(){
+  // login(){
+  //   var acnum=this.acno
+  //   var psw=this.psw
+  //   var userDetails=this.userDetails
 
-  }
-  acnoChange(event:any){
-    this.acno=event.target.value
+  //   if(acnum in userDetails){
+  //     if(psw==userDetails[acnum]["password"]){
+  //       alert("sucess")
+  //     }
+  //     else{
+  //       alert("incurrect password")
+  //     }
+  //   }
+  //   else{
+  //     alert("acno incurrect or not register yet")
+  //   }
+  // }
+  login(a:any,b:any){
+    var acnum=a.value
+    var psw=b.value
+    var userDetails=this.userDetails
 
+    if(acnum in userDetails){
+      if(psw==userDetails[acnum]["password"]){
+        alert("sucess")
+      }
+      else{
+        alert("incurrect password")
+      }
+    }
+    else{
+      alert("acno incurrect or not register yet")
+    }
   }
-  passChange(event:any){
-    this.psw=event.target.value
-  }
+  // acnoChange(event:any){
+  //   this.acno=event.target.value
+
+  // }
+  // passChange(event:any){
+  //   this.psw=event.target.value
+  // }
 
 }
