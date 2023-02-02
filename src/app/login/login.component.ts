67f61c15a74b3d899a10488fcb0b6152fdccfd09
unknown <dinodavis18@gmail.com>
2023-02-02 08:38:58 +0530

    third bank commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -9,46 +11,23 @@ export class LoginComponent implements OnInit {
 
   data="Your Perfect Banking Partner"
 
-  // acno=""
-  // psw=""
+  acno=''
+  psw=''
 
-  userDetails:any={
-    1000:{acno:1000,username:"anu",password:"abc123",balance:0},
-    1001:{acno:1001,username:"amal",password:"abc123",balance:0},
-    1003:{acno:1003,username:"arun",password:"abc123",balance:0},
-    1004:{acno:1004,username:"akil",password:"abc123",balance:0},
-  }
-
-  constructor(){ }
+  constructor(private router:Router,private ds:DataService){ }
 
   ngOnInit(): void {
   }
 
-  // login(){
-  //   var acnum=this.acno
-  //   var psw=this.psw
-  //   var userDetails=this.userDetails
-
-  //   if(acnum in userDetails){
-  //     if(psw==userDetails[acnum]["password"]){
-  //       alert("sucess")
-  //     }
-  //     else{
-  //       alert("incurrect password")
-  //     }
-  //   }
-  //   else{
-  //     alert("acno incurrect or not register yet")
-  //   }
-  // }
-  login(a:any,b:any){
-    var acnum=a.value
-    var psw=b.value
-    var userDetails=this.userDetails
+  login(){
+    var acnum=this.acno
+    var psw=this.psw
+    var userDetails=this.ds.userDetails
 
     if(acnum in userDetails){
       if(psw==userDetails[acnum]["password"]){
         alert("sucess")
+        this.router.navigateByUrl('dashboard')
       }
       else{
         alert("incurrect password")
@@ -58,12 +37,4 @@ export class LoginComponent implements OnInit {
       alert("acno incurrect or not register yet")
     }
   }
-  // acnoChange(event:any){
-  //   this.acno=event.target.value
-
-  // }
-  // passChange(event:any){
-  //   this.psw=event.target.value
-  // }
-
 }
